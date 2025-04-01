49287bfca6c8532b2787c28cb9ea3a7e7ca3552d
Clayton Wingfield <clayton.wingfield@okstate.edu>
2025-04-01 14:47:53 -0500

    Removed Backend

--- backend/backend-scorecard/src/api/manager/routes/manager.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * manager router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::manager.manager');
--- backend/backend-scorecard/src/api/scorecard/controllers/scorecard.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * scorecard controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::scorecard.scorecard');
